e82843b03b20383cba097f6383ef4a750a8d29dd
Ehab97 <ehabreda04@gmail.com>
2024-04-28 15:50:30 +0300

    test env

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,20 @@ import UserForm from "./components/user/UserForm";
 import UserList from "./components/user/UserList";
 
 function App() {
-   const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
-   const onAddUser = (user) => {
-      setUsers((users) => [...users, user]);
-   };
-
-   return (
-      <div className="app">
-         <UserForm  onAddUser={onAddUser}/>
-         <hr/>
-         <UserList users={users}/>
-      </div>
-   );
+  const onAddUser = (user) => {
+    setUsers((users) => [...users, user]);
+  };
+  console.log(import.meta.env);
+  return (
+    <div className="app">
+      <h2>{import.meta.env.VITE_BASE_APP_NAME}</h2>
+      <UserForm onAddUser={onAddUser} />
+      <hr />
+      <UserList users={users} />
+    </div>
+  );
 }
 
 export default App;
