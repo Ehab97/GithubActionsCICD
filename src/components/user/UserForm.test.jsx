4dc5d01cb7b4bd217797c4ead184b01f2ffd8455
Ehab97 <ehabreda04@gmail.com>
2024-06-21 21:14:41 +0300

    break test

--- src/components/user/UserForm.test.jsx
+++ src/components/user/UserForm.test.jsx
@@ -6,14 +6,14 @@ import UserList from "./UserList";
 import { vi } from "vitest";
 
 test("It shows Name and Email Inputs and Submit Button", async () => {
-   //render Component
-   render(<UserForm />);
-   //manipulate the Component or find element in it
-   const inputs = screen.getAllByRole("textbox");
-   const button = screen.getByRole("button");
-   //assertion make sure Component what it's suppose to doing
-   expect(inputs).toHaveLength(2);
-   expect(button).toBeInTheDocument();
+  //render Component
+  render(<UserForm />);
+  //manipulate the Component or find element in it
+  const inputs = screen.getAllByRole("textbox");
+  const button = screen.getByRole("button");
+  //assertion make sure Component what it's suppose to doing
+  expect(inputs).toHaveLength(3);
+  expect(button).toBeInTheDocument();
 });
 
 // test("it shows when OnUserAdded submitted", async () => {
@@ -35,54 +35,54 @@ test("It shows Name and Email Inputs and Submit Button", async () => {
 // });
 
 test("it shows when OnUserAdded submitted", async () => {
-   ``;
-   const mock = vi.fn();
-   // 1. Render the Component
-   render(<UserForm onAddUser={mock} />);
+  ``;
+  const mock = vi.fn();
+  // 1. Render the Component
+  render(<UserForm onAddUser={mock} />);
 
-   // 2. Manipulate the Component or find elements in it
-   const nameInput = screen.getByRole("textbox", {
-      name: /Enter name/,
-   });
-   const emailInput = screen.getByRole("textbox", {
-      name: /Enter email/,
-   });
+  // 2. Manipulate the Component or find elements in it
+  const nameInput = screen.getByRole("textbox", {
+    name: /Enter name/,
+  });
+  const emailInput = screen.getByRole("textbox", {
+    name: /Enter email/,
+  });
 
-   // Simulate user interactions
-   fireEvent.input(nameInput, { target: { value: "ehab" } });
-   fireEvent.input(emailInput, { target: { value: "ehab@ehab.com" } });
+  // Simulate user interactions
+  fireEvent.input(nameInput, { target: { value: "ehab" } });
+  fireEvent.input(emailInput, { target: { value: "ehab@ehab.com" } });
 
-   // Find and click the button
-   const button = screen.getByRole("button");
-   fireEvent.click(button);
+  // Find and click the button
+  const button = screen.getByRole("button");
+  fireEvent.click(button);
 
-   // 3. Assertion: Make sure the Component does what it's supposed to do
-   expect(mock).toHaveBeenCalled();
-   expect(mock).toHaveBeenCalledWith({ name: "ehab", email: "ehab@ehab.com" });
+  // 3. Assertion: Make sure the Component does what it's supposed to do
+  expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledWith({ name: "ehab", email: "ehab@ehab.com" });
 });
 
 test("empty two inputs when form submitted", () => {
-   const mock = vi.fn();
-   // 1. Render the Component
-   render(<UserForm onAddUser={mock} />);
+  const mock = vi.fn();
+  // 1. Render the Component
+  render(<UserForm onAddUser={mock} />);
 
-   // 2. Manipulate the Component or find elements in it
-   const nameInput = screen.getByRole("textbox", {
-      name: /Enter name/,
-   });
-   const emailInput = screen.getByRole("textbox", {
-      name: /Enter email/,
-   });
+  // 2. Manipulate the Component or find elements in it
+  const nameInput = screen.getByRole("textbox", {
+    name: /Enter name/,
+  });
+  const emailInput = screen.getByRole("textbox", {
+    name: /Enter email/,
+  });
 
-   // Simulate user interactions
-   fireEvent.input(nameInput, { target: { value: "ehab" } });
-   fireEvent.input(emailInput, { target: { value: "ehab@ehab.com" } });
+  // Simulate user interactions
+  fireEvent.input(nameInput, { target: { value: "ehab" } });
+  fireEvent.input(emailInput, { target: { value: "ehab@ehab.com" } });
 
-   // Find and click the button
-   const button = screen.getByRole("button");
-   fireEvent.click(button);
+  // Find and click the button
+  const button = screen.getByRole("button");
+  fireEvent.click(button);
 
-   // 3. Assertion: Make sure the Component does what it's supposed to do
-   expect(nameInput).toHaveValue("");
-   expect(emailInput).toHaveValue("");
+  // 3. Assertion: Make sure the Component does what it's supposed to do
+  expect(nameInput).toHaveValue("");
+  expect(emailInput).toHaveValue("");
 });
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [react()],
-   test: {
-      globals: true,
-      environment: "jsdom",
-      setupFiles: ["src/setupTest.js"],
-   },
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["src/setupTest.js"],
+    reporters: ["json"],
+    outputFile: "test.json",
+  },
 });
